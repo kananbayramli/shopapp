2873247accc9e9d464944f2999e59644f2e813fe
kananbayramli <bayramli-kenan@mail.ru>
2024-04-19 12:45:59 +0400

    Search form : Get method

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -18,12 +18,17 @@ namespace shopapp.ui.Controllers
             return View(product);
         }
 
-        public IActionResult List(int? id)
+        public IActionResult List(int? id, string q)
         {
             var products = ProductRepository.Products;
             if(id!=null)
             {
                 products = products.Where(p => p.CategoryId == id).ToList();
+            }
+
+            if(!string.IsNullOrEmpty(q))
+            {
+                products = products.Where(p => p.Name.ToLower().Contains(q) || p.Description.ToLower().Contains(q)).ToList();
             }
 
             var productViewModel = new ProductViewModel()
