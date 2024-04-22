13a8771bff6077b22aafd8dc38c7db82fdf19d95
kananbayramli <bayramli-kenan@mail.ru>
2024-04-22 19:06:31 +0400

    Product details

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using shopapp.business.Abstract;
+using shopapp.entity;
 using shopapp.ui.ViewModels;
 
 namespace shopapp.ui.Controllers
@@ -21,5 +22,21 @@ namespace shopapp.ui.Controllers
             };
             return View(productViewModel);
         }
+
+        public IActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            Product product = _productService.GetById((int)id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
     }
 }
