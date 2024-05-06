314f8f1172a6c054b0710251419346ac6b8f5abf
kananbayramli <bayramli-kenan@mail.ru>
2024-05-06 19:55:12 +0400

    Login works

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using shopapp.ui.Identity;
 using shopapp.ui.Models;
 
@@ -17,10 +18,43 @@ namespace shopapp.ui.Controllers
             _userManager = userManager;
         }
 
-        public IActionResult Login()
+        public IActionResult Login(string ReturnUrl=null)
         {
-            return View();
+            return View(new LoginModel()
+            {
+                ReturnUrl = ReturnUrl
+            });
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            //var user = await _userManager.FindByNameAsync(model.UserName);
+            var user = await _userManager.FindByEmailAsync(model.Email);
+
+            if(user == null)
+            {
+                ModelState.AddModelError("", "Your username is not true");
+                return View(model);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(user, model.Password,true, false);
+
+            if (result.Succeeded)
+            {
+                return Redirect(model.ReturnUrl??"~/");
+            }
+            ModelState.AddModelError("", "Your username or password is not true");
+            return View(model);
+        }
+
+
+
 
         public IActionResult Register()
         {
