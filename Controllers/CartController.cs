01b7fddb6ca9652ac46061e2d4a587136d33520c
kananbayramli <bayramli-kenan@mail.ru>
2024-05-10 23:17:21 +0400

    Add To Basket buttons

---
+++ Controllers/CartController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using shopapp.entity;
+
+namespace shopapp.ui.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddToCart()
+        {
+            return View();
+        }
+    }
+}
