a3c642a19d9ee559e30c1d4e41478c0899321db8
kananbayramli <bayramli-kenan@mail.ru>
2024-05-15 22:52:22 +0400

    ConnectionString to Appsettings

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,11 @@ namespace shopapp.ui
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationContext>(options => options.UseSqlite("Data Source=shopDb"));
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlite(_configuration.GetConnectionString("SqliteConnection")));
+            services.AddDbContext<ShopContext>(options => options.UseSqlite(_configuration.GetConnectionString("SqliteConnection")));
+
+
+
             services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<ApplicationContext>().AddDefaultTokenProviders();
 
             services.Configure<IdentityOptions>(options =>{
